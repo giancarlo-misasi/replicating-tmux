93249642386e1b1dd63882aebb2a73c79a0751e5
Giancarlo <giancarlo-misasi@users.noreply.github.com>
2024-12-23 23:31:36 -0800

    refactored main loop

--- src/main.rs
+++ src/main.rs
@@ -5,138 +5,123 @@ pub mod pty;
 
 use pty::Pty;
 use std::io::{self, stdin, stdout, Read, Write};
-use std::{
-    process::exit,
-    sync::{
-        mpsc::{channel, Sender},
-        Arc, Mutex,
-    },
-    thread,
-};
+use std::{sync::mpsc::channel, thread};
+use termion::{clear, cursor};
 use termion::{raw::IntoRawMode, terminal_size};
 
-#[derive(Clone)]
-pub(crate) struct WriterWrapper {
-    writer: Arc<Mutex<Box<dyn Write + Send>>>,
-}
-
-impl WriterWrapper {
-    pub fn new(writer: Box<dyn Write + Send>) -> Self {
-        Self {
-            writer: Arc::new(Mutex::new(writer)),
-        }
-    }
-}
-
-impl std::io::Write for WriterWrapper {
-    fn write(&mut self, buf: &[u8]) -> std::io::Result<usize> {
-        let mut writer = self.writer.lock().unwrap();
-        writer.write(buf)
-    }
-
-    fn flush(&mut self) -> std::io::Result<()> {
-        let mut writer = self.writer.lock().unwrap();
-        writer.flush()
-    }
-}
-
-#[derive(Clone)]
-pub(crate) struct ReaderWrapper {
-    reader: Arc<Mutex<Box<dyn Read + Send>>>,
-}
-
-impl ReaderWrapper {
-    pub fn new(reader: Box<dyn Read + Send>) -> Self {
-        Self {
-            reader: Arc::new(Mutex::new(reader)),
-        }
-    }
-
-    fn read(&mut self, buf: &mut [u8]) -> io::Result<usize> {
-        let mut reader = self.reader.lock().unwrap();
-        reader.read(buf)
-    }
-}
-
-const BUFSIZE: usize = 128 * 128;
-
-fn read_from_shell(mut reader: ReaderWrapper, tx: Sender<Vec<u8>>) {
-    thread::spawn(move || {
-        let mut buf: Vec<u8> = vec![0; BUFSIZE]; // Reusable buffer
-        loop {
-            match reader.read(&mut buf) {
-                Ok(bytes_read) => {
-                    let data = buf[..bytes_read].to_vec();
-                    if let Err(e) = tx.send(data) {
-                        eprintln!("Failed to send message: {}", e);
-                        break;
-                    }
-                }
-                _ => {
-                    std::thread::sleep(std::time::Duration::from_millis(10));
-                }
-            }
-        }
-    });
-}
-
-fn pass_input_to_shell(mut writer: WriterWrapper) {
-    thread::spawn(move || {
-        let mut stdin = stdin().lock();
-        let mut buf = [0u8; 1024];
-        loop {
-            if let Ok(bytes_read) = stdin.read(&mut buf) {
-                if bytes_read > 0 && buf[0] == 0x11 {
-                    exit(0);
-                }
-
-                let _ = writer.write(&buf[..bytes_read]);
-                let _ = writer.flush();
-            }
-        }
-    });
-}
-
-fn main() {
-    println!("start");
-
+fn run() -> io::Result<()> {
     // setup
-    let cmd = std::process::Command::new("zsh");
-    let pty = Pty::open(cmd).unwrap();
-    let reader = ReaderWrapper::new(pty.try_clone_reader().unwrap());
-    let writer = WriterWrapper::new(pty.take_writer().unwrap());
     let (tx, rx) = channel();
+    let (exit_tx, exit_rx) = channel();
+    let pty = Pty::open(std::process::Command::new("zsh"))?;
+    let mut reader = pty.try_clone_reader()?;
+    let mut writer = pty.take_writer()?;
 
-    read_from_shell(reader.clone(), tx.clone());
-    pass_input_to_shell(writer.clone());
-
-    let mut stdout = stdout().into_raw_mode().unwrap();
-    write!(
-        stdout,
-        "{}{}",
-        termion::clear::All,
-        termion::cursor::Goto(1, 1)
-    )
-    .unwrap();
-
-    // capture initial terminal size to check for changes
+    // resize the client
     let (mut cols, mut rows) = terminal_size().unwrap();
     pty.resize(rows, cols).unwrap();
 
-    // start output loop
-    loop {
-        let output = rx.recv().unwrap();
-        if let Ok(str) = std::str::from_utf8(&output) {
-            write!(stdout, "{}", str).unwrap();
-            stdout.flush().unwrap();
+    // pipe pty output to tx
+    let exit_tx_clone = exit_tx.clone();
+    thread::spawn(move || {
+        let mut buf = [0u8; 128 * 128]; // at least rows * cols buffer size
+        loop {
+            match reader.read(&mut buf) {
+                Ok(bytes_read) => {
+                    if bytes_read == 0 {
+                        eprintln!("worker pty has been closed, terminating.");
+                        break;
+                    }
+
+                    let data = buf[..bytes_read].to_vec();
+                    if let Err(e) = tx.send(data) {
+                        eprintln!("failed to pipe pty output to tx: {}", e);
+                        break;
+                    }
+                }
+                Err(e) => {
+                    eprintln!("failed to read pty output: {}", e);
+                    break;
+                }
+            }
+        }
+        let _ = exit_tx_clone.send(0);
+    });
+
+    // pipe stdin to pty
+    let exit_tx_clone = exit_tx.clone();
+    thread::spawn(move || {
+        let mut stdin = stdin().lock();
+        let mut buf = [0u8; 128]; // at least one row at a time
+        loop {
+            match stdin.read(&mut buf) {
+                Ok(bytes_read) => {
+                    if bytes_read == 0 {
+                        eprintln!("stdin has been closed, terminating.");
+                        break;
+                    }
+
+                    if let Err(e) = writer.write(&buf[..bytes_read]) {
+                        eprintln!("failed to pipe stdin to pty: {}", e);
+                        break;
+                    }
+                }
+                Err(e) => {
+                    eprintln!("failed to read stdin: {}", e);
+                    break;
+                }
+            }
+        }
+        let _ = exit_tx_clone.send(1);
+    });
+
+    // pipe rx to stdout
+    let exit_tx_clone = exit_tx.clone();
+    thread::spawn(move || {
+        let mut stdout = stdout().into_raw_mode().unwrap();
+        write!(stdout, "{}{}", clear::All, cursor::Goto(1, 1)).unwrap();
+
+        loop {
+            match rx.recv() {
+                Ok(output) => match std::str::from_utf8(&output) {
+                    Ok(str) => {
+                        if let Err(e) = write!(stdout, "{}", str) {
+                            eprintln!("failed to pipe rx to stdout: {}", e);
+                            break;
+                        }
+                        if let Err(e) = stdout.flush() {
+                            eprintln!("failed to flush rx to stdout: {}", e);
+                            break;
+                        }
+                    }
+                    Err(e) => {
+                        eprintln!("failed to parse utf8: {}", e);
+                        break;
+                    }
+                },
+                Err(e) => {
+                    eprintln!("failed to read rx: {}", e);
+                    break;
+                }
+            }
+
+            if let Ok((c, r)) = terminal_size() {
+                if c != cols || r != rows {
+                    (rows, cols) = (r, c);
+                    let _ = pty.resize(rows, cols); // ignore resize failures
+                }
+            }
         }
 
-        // check if we need a resize
-        let (c, r) = terminal_size().unwrap();
-        if c != cols || r != rows {
-            rows = r;
-            cols = c;
-            pty.resize(rows, cols).unwrap();
-        }
-    }
+        let _ = exit_tx_clone.send(2);
+    });
+
+    // wait until any of these threads terminate
+    exit_rx.recv().unwrap();
+
+    Ok(())
+}
+
+fn main() {
+    run().unwrap()
 }
